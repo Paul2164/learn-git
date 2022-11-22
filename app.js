1e88341b926ae1f53c57539d2951fefe054c7b9e
Paul <fpaul847@gmail.com>
2022-11-22 13:01:16 +0100

    commit 2

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const random = Math.random()
 console.log(random)
+
+function addNumbers (max,min) {
+
+    return Math.random() * (max-min) + min
+}
+
+let numbers = addNumbers(5,10)
+console.log(numbers)
